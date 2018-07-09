9f789b2f81a6e13209f7685eaa4a374018e74f8d
Robin Gareus <robin@gareus.org>
2018-07-09 19:57:43 +0200

    Ongoing disk-reader + playback buffering work

--- libs/ardour/ardour/session.h
+++ libs/ardour/ardour/session.h
@@ -1439,7 +1439,6 @@ private:
 			PostTransportReverse|
 			PostTransportCurveRealloc|
 			PostTransportAudition|
-			PostTransportStop|
 			PostTransportClearSubstate);
 
 	gint _post_transport_work; /* accessed only atomic ops */
